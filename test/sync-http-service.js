094916d434ab97b57060c7eb3df5ea530909e9e4
KevFan <kevin_fan@hotmail.co.uk>
2017-12-22 10:47:05 +0000

    sync-http-client reimplementation to work with jwt

--- test/sync-http-service.js
+++ test/sync-http-service.js
@@ -1,14 +1,33 @@
-let request = require('sync-request');
+const request = require('sync-request');
 
 class SyncHttpService {
 
   constructor(baseUrl) {
     this.baseUrl = baseUrl;
+    this.authHeadder = null;
+  }
+
+  setAuth(url, user) {
+    const res = request('POST', this.baseUrl + url, { json: user });
+    if (res.statusCode === 201) {
+      const payload = JSON.parse(res.getBody('utf8'));
+      if (payload.success) {
+        this.authHeadder = { Authorization: 'bearer ' + payload.token, };
+        return true;
+      }
+    }
+
+    this.authHeadder = null;
+    return false;
+  }
+
+  clearAuth() {
+    this.authHeadder = null;
   }
 
   get(url) {
     let returnedObj = null;
-    let res = request('GET', this.baseUrl + url);
+    const res = request('GET', this.baseUrl + url, { headers: this.authHeadder });
     if (res.statusCode < 300) {
       returnedObj = JSON.parse(res.getBody('utf8'));
     }
@@ -18,7 +37,7 @@ class SyncHttpService {
 
   post(url, obj) {
     let returnedObj = null;
-    let res = request('POST', this.baseUrl + url, { json: obj });
+    const res = request('POST', this.baseUrl + url, { json: obj, headers: this.authHeadder });
     if (res.statusCode < 300) {
       returnedObj = JSON.parse(res.getBody('utf8'));
     }
@@ -26,8 +45,8 @@ class SyncHttpService {
     return returnedObj;
   }
 
-  delete (url) {
-    let res = request('DELETE', this.baseUrl + url);
+  delete(url) {
+    const res = request('DELETE', this.baseUrl + url, { headers: this.authHeadder });
     return res.statusCode;
   }
 }
